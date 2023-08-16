0bbb91cd9cc1c998a40613b79c315da204a4aa35
mauriciomr1985 <mauriciomr1985@gmail.com>
2023-08-16 12:39:54 -0400

    Check 'chr' presence in BAM_sorted files before proceeding

--- split_bam.sh
+++ split_bam.sh
@@ -15,8 +15,17 @@ SORTED_BAM=$(dirname ${BAM})/$(basename ${BAM%.bam})'_sorted.bam'
 FOLDER=$(dirname ${OUTPUT_DIR})
 FASTQ=$(basename ${BAM%.bam})
 
+# Check for the presence of 'chr' in SORTED_BAM
+if samtools view ${SORTED_BAM} | grep -q "chr"; then
+    # 'chr' is present in the BAM file
+    CHR_PREFIX="chr"
+else
+    # 'chr' is not present in the BAM file
+    CHR_PREFIX=""
+fi
+
 # Loop on chromosomes
 for CHR in `seq 1 22` X Y ; do
-    samtools view -bh ${SORTED_BAM} chr${CHR} > ${FOLDER}/${FASTQ}_chr${CHR}.bam
+    samtools view -bh ${SORTED_BAM} ${CHR_PREFIX}${CHR} > ${FOLDER}/${FASTQ}_chr${CHR}.bam
 done
 
